4dc6e20ddae9bf2464470ad6b23d672af5af10b3
Jay Dave <159456849+DaveJay70@users.noreply.github.com>
2025-05-17 00:24:49 +0530

    Add files via upload

---
+++ Logical/ArmstrongNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        sc.close();
+
+        int original = number;
+        int digits = String.valueOf(number).length();
+        System.out.println("Digits:"+digits);
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is NOT an Armstrong number.");
+        }
+    }
+}
